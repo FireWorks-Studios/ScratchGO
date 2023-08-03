e1d042f8ad4feb0107858e04bd8f400c0db7175c
weitude <eric.c.w.lin@gmail.com>
2023-08-03 13:55:32 +0800

    change title to ScratchGO beta

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ScratchGO",
+  title: "ScratchGO beta",
   description: "Play ANY keyboard-controlled Scratch game on your phone.",
 };
 
